29ef014b1094c9a185bad0e85954d20de4d740ac
thiernobarry <tmabarry@yahoo.com>
2024-07-10 19:31:45 -0400

    Add Extract class

---
+++ Extract.py
@@ -0,0 +1,12 @@
+class Extract:
+    def from_csv(self):
+        pass
+ 
+    def from_json(self):
+        pass
+ 
+    def from_mysql(self):
+        pass
+ 
+    def from_postgres(self):
+        pass
