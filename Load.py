cf3b2b184a24a5e5b3b45ff66edf93a274cc2759
thiernobarry <tmabarry@yahoo.com>
2024-07-13 15:08:04 -0400

    Add class Load

---
+++ Load.py
@@ -0,0 +1,13 @@
+class Load:
+    # load class must be able to save the data to the target format
+    def to_csv(self):
+        pass
+
+    def to_json(self):
+        pass
+
+    def to_mysql(self):
+        pass
+
+    def to_postgres(self):
+        pass
